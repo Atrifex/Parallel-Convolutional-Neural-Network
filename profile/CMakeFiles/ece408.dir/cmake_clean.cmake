54efeae395e7081c28778e312ef7c00fd4933a8b
Rishi Thakkar <rishirt.us@gmail.com>
2016-12-19 00:55:52 -0600

    profile for base imp added

---
+++ profile/CMakeFiles/ece408.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ece408.dir/src/ece408_generated_main.cu.o"
+  "ece408.pdb"
+  "ece408"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ece408.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
